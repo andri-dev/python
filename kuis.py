7110c4943f2b28aac9534fbd881062886f6b209c
andrijunaedi <andrijunaedi126@gmail.com>
2020-10-24 23:16:29 +0700

    progress-1

---
+++ kuis.py
@@ -0,0 +1,17 @@
+# a = 6
+# b = 4
+
+# c = "Hasilnya " + str (a + b) + "6" * 3
+
+# print(c)
+
+# a = True ** 6
+# # b = "Hello" + 3" --> error
+# c = "Hello" * 3
+# d = "Ini adalah " + str(5) +  " dalam bentuk string."
+
+# print(c)
+
+list_a = range(1,10,2)
+x = [ [a**2, a**3] for a in list_a]
+print(x)
